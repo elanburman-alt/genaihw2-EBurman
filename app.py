6a58fd9f066695b0ce5064e1fe7075636aba60ed
Elan L Burman <eburman3@jh.edu>
2026-04-10 14:18:07 -0400

    Step 4: Working prototype in app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ API_KEY = "AIzaSyBDveD6t2_oQ4T4gREQ5J1JaNcF1rc2XJw"
 MODEL_NAME = "gemini-2.5-flash"
 
 # --- SYSTEM PROMPT ---
-SYSTEM_PROMPT = """Draft a donor follow-up email based on this information."""
+SYSTEM_PROMPT = """You are a world-class major gifts professional fundraiser helping draft warm, personalized donor 
+follow-up emails. Do not invent facts. Keep to 3-4 paragraphs."""
 
 # --- DONOR INPUTS ---
 test_inputs = [
